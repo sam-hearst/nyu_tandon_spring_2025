1b1804d5675c5818c155fb06c951c09cab8c9022
Sam Hearst <samhearst1013@gmail.com>
2025-04-25 17:04:34 -0400

    q 4

--- week_2/q_2.cpp
+++ week_2/q_2.cpp
@@ -4,9 +4,30 @@ using namespace std;
 int main() {
     int numDollars;
     int numCents;
+    int currTotalCents;
+    int numQuarters;
+    int numDimes;
+    int numNickels;
+    int numPennies;
 
-    cout << "Please enter your amount in the format of dollars and cents seperate by a space:" << endl;
+    cout << "Please enter your amount in the format of dollars and cents separated by a space:" << endl;
     cin>>numDollars>>numCents;
+
+    cout <<numDollars<<" dollars and "<<numCents<<" cents are:"<<endl;
+
+    currTotalCents = (numDollars * 100) + numCents;
+    numQuarters = currTotalCents / 25;
+    currTotalCents = currTotalCents % 25;
+
+    numDimes = currTotalCents / 10;
+    currTotalCents = currTotalCents % 10;
+
+    numNickels = currTotalCents / 5;
+    currTotalCents = currTotalCents % 5;
+
+    numPennies = currTotalCents / 1;
+
+    cout <<numQuarters<<" quarters, "<<numDimes<<" dimes, "<<numNickels<<" nickels and "<<numPennies<<" pennies "<<endl;
 
     return 0;
 }
---
+++ week_2/q_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int posInt1;
+    int posInt2;
+
+    cout << "Please enter two positive integers, separated by a space:" << endl;
+    cin>>posInt1>>posInt2;
+
+    
+    return 0;
+}
